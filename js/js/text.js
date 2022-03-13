7a8c8d1be5bef329f8f5a6a43aff488d51e4db11
gulshan25 <g.r.tanny@gmail.com>
2022-03-13 21:18:03 +0600

    first commit

---
+++ js/js/text.js
@@ -0,0 +1,9 @@
+function myStyling() {
+    document.getElementById('btn1').style.padding = '20px';
+    document.getElementById('btn2').style.padding = '20px';
+    document.getElementById('btn2').style.margin = '20px';
+    document.getElementById('date').style.fontWeight = 'bold';
+    document.getElementById('date').style.color = 'green';
+    document.getElementById('btn3').style.backgroundColor = 'black';
+    document.getElementById('btn3').style.color = 'white';
+}
