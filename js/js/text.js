25c56cba0ff423058b76ff645670137c58ebbea0
gulshan25 <g.r.tanny@gmail.com>
2022-03-14 18:54:50 +0600

    javascript

--- js/js/text.js
+++ js/js/text.js
@@ -1,9 +1,29 @@
 function myStyling() {
     document.getElementById('btn1').style.padding = '20px';
     document.getElementById('btn2').style.padding = '20px';
+    document.getElementById('btn3').style.padding = '20px';
     document.getElementById('btn2').style.margin = '20px';
+    document.getElementById('btn1').style.margin = '20px';
+    document.getElementById('btn3').style.margin = '20px';
     document.getElementById('date').style.fontWeight = 'bold';
     document.getElementById('date').style.color = 'green';
     document.getElementById('btn3').style.backgroundColor = 'black';
     document.getElementById('btn3').style.color = 'white';
+    document.getElementById('date').innerHTML = Date();
+    document.getElementById('date').innerHTML = 'switch';
+    document.getElementById('date').style.fontSize = '25px';
+    document.getElementById('date').style.alignItems = 'center';
 }
+
+// myStyling();
+// let x = 7;
+// if (true) {
+//     let x = 6;
+//     console.log(x);
+// }
+
+// console.log(x);
+
+carName = 'volvo';
+let carName;
+console.log(carName);
