878a34488a58e739e6bd93a3ae640b0a989c7015
gulshan25 <g.r.tanny@gmail.com>
2022-03-15 17:44:49 +0600

    javascript

---
+++ js/js/main.js
@@ -0,0 +1,17 @@
+
+// let 
+
+// let a =3;
+// let b = (100 + 50) * a;
+// document.getElementById("pra2").innerHTML = b;
+
+// let x = 16 + "volvo";
+//         document.getElementById("pra2").innerHTML = x;
+
+// let  y = "volvo" + 16 + 4;
+// document.getElementById("pra2").innerHTML = y;
+
+let c;
+c = 5;
+
+
--- js/js/text.js
+++ js/js/text.js
@@ -1,19 +1,19 @@
-function myStyling() {
-    document.getElementById('btn1').style.padding = '20px';
-    document.getElementById('btn2').style.padding = '20px';
-    document.getElementById('btn3').style.padding = '20px';
-    document.getElementById('btn2').style.margin = '20px';
-    document.getElementById('btn1').style.margin = '20px';
-    document.getElementById('btn3').style.margin = '20px';
-    document.getElementById('date').style.fontWeight = 'bold';
-    document.getElementById('date').style.color = 'green';
-    document.getElementById('btn3').style.backgroundColor = 'black';
-    document.getElementById('btn3').style.color = 'white';
-    document.getElementById('date').innerHTML = Date();
-    document.getElementById('date').innerHTML = 'switch';
-    document.getElementById('date').style.fontSize = '25px';
-    document.getElementById('date').style.alignItems = 'center';
-}
+// function myStyling() {
+//     document.getElementById('btn1').style.padding = '20px';
+//     document.getElementById('btn2').style.padding = '20px';
+//     document.getElementById('btn3').style.padding = '20px';
+//     document.getElementById('btn2').style.margin = '20px';
+//     document.getElementById('btn1').style.margin = '20px';
+//     document.getElementById('btn3').style.margin = '20px';
+//     document.getElementById('date').style.fontWeight = 'bold';
+//     document.getElementById('date').style.color = 'green';
+//     document.getElementById('btn3').style.backgroundColor = 'black';
+//     document.getElementById('btn3').style.color = 'white';
+//     document.getElementById('date').innerHTML = Date();
+//     document.getElementById('date').innerHTML = 'switch';
+//     document.getElementById('date').style.fontSize = '25px';
+//     document.getElementById('date').style.alignItems = 'center';
+// }
 
 // myStyling();
 // let x = 7;
@@ -36,3 +36,4 @@ document.getElementById("demo").innerHTML = b;
 let text1 = 'Hello,';
 text1 += 'World!';
 document.getElementById("pra2").innerHTML = text1;
+
