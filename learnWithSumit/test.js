550c159fc6b6d8de02d370b2b0ff414d0c392733
gulshan25 <g.r.tanny@gmail.com>
2022-03-16 21:38:22 +0600

    practice

---
+++ learnWithSumit/test.js
@@ -0,0 +1,40 @@
+//function sleep (name, time) {
+//    console.log (name + ' is sleeping from ' + time);
+//    return 5;
+//}
+//
+////console.log(name);
+//sleep('rahim', '11 pm');
+////sleep('karim', '11 pm');  
+////sleep('jasim', '11 pm');
+//
+//console.log(4);
+//
+//function myFunction(a, b) {
+////    console.log("hello");
+//}
+//let x = myFunction(4, 3);
+//
+//
+//let x = myFunction(4, 3);
+//
+//function myFunction(a, b) {
+//    return a * b;
+//}
+
+const car = {
+    name: "volvo",
+    model: 500,
+    weight: "850kg",
+    color: "white",
+    start: function () {
+        console.log("car has started");
+    },
+    drive: function () {
+        console.log("car is driving");      
+    },
+};
+
+//console.log(car.weight);
+
+console.log(car[model]);
