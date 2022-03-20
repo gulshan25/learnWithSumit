b5b68a92c9a272a6e35c33c218d6959ce751948b
gulshan25 <g.r.tanny@gmail.com>
2022-03-20 14:37:42 +0600

    practice

--- dist/index.997f4b76.js
+++ dist/index.997f4b76.js
@@ -1,31 +1,17 @@
 let str1 = "Apple, Banana, Kiwi";
 console.log(str1.slice(7, 13));
-
 let substr = "Apple, Banana, Kiwi";
 console.log(substr.slice(7, 13));
-
 let str2 = "Apple, Banana, Kiwi";
 console.log(str2.slice(-12, -6));
-
 let num1 = "1, 2, 3";
 console.log(num1.slice(4, 3));
-
 const num2 = "1, 2, 3";
 console.log(num2.slice(-1, -1));
-
 let str = "Apple, banana, kiwi";
 let banana = str.slice(7, 13);
 console.log(banana);
-
 let text = "Hello world";
 console.log(text.startsWith("world, 6"));
 
-let name = "mosh";
-console.log(name);
-
-console.log("Hello");
-
-let firstName = "John";
-let lastName = "doe";
-
-console.log(firstName);
+//# sourceMappingURL=index.997f4b76.js.map
---
+++ text.js
@@ -0,0 +1,7 @@
+let a = "Hello world";
+let b = 12;
+
+console.log(isNaN(a));
+
+const d = new Date(2022, 3, 20);
+document.getElementById("p1").innerHTML = d;
